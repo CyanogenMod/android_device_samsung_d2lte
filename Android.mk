78af10a90e531b54988ad98cbcc0d0558b318bb4
Steve Kondik <shade@chemlab.org>
2013-12-21 19:32:05 -0800

    d2: Remove modem links scripts
     * Create the symlinks ourselves at build time. Remounting the system
       partition to do this is just stupid.
    
    Change-Id: I224c20af820554661fe027e5835b18cb5589c694
    
    Conflicts:
    	Android.mk
    	rootdir/etc/init.qcom.mdm_links.sh
    	rootdir/etc/init.qcom.modem_links.sh
    
    Change-Id: I3c01fe3cc66836225f55de9741339206099aecb2

--- Android.mk
+++ Android.mk
@@ -25,7 +25,47 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(BOARD_VENDOR),samsung)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
 ifneq ($(filter d2lte apexqtmo expressatt d2dcm,$(TARGET_DEVICE)),)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+# Create /firmware-mdm links
+FIRMWARE_MDM_IMAGES := \
+    acdb.mbn \
+    apps.mbn \
+    dsp1.mbn dsp2.mbn dsp3.mbn \
+    efs1.mbn efs2.mbn efs3.mbn \
+    mdm_acdb.img \
+    rpm.mbn \
+    sbl1.mbn \
+    sbl2.mbn
+
+FIRMWARE_MDM_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware-msm/,$(notdir $(FIRMWARE_MDM_IMAGES)))
+$(FIRMWARE_MDM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware-mdm/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MDM_SYMLINKS)
+
+
+# Create /firmware links
+FIRMWARE_MDM_IMAGES := \
+    q6.b00 q6.b01 q6.b03 q6.b04 q6.b05 q6.b06 q6.mdt \
+    tzapps.b00 tzapps.b01 tzapps.b02 tzapps.b03 tzapps.mdt \
+    vidc.b00 vidc.b01 vidc.b02 vidc.b03 vidc.mdt
+
+FIRMWARE_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_IMAGES)))
+$(FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_SYMLINKS)
+
 endif
 endif
 endif
