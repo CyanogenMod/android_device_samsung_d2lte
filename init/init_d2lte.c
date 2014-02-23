92a9a84f521f59c18388eb27473dcdcfc90f57a6
Dan Pasanen <dan.pasanen@gmail.com>
2014-02-23 10:27:40 -0600

    Revert "init_d2lte: remove ro.telephony.default_cdma_sub from r530s"
    This reverts commit bd077567f4fd75cba0bdf1eb5a9a0a4c0ac83488.
    
    Change-Id: I6460e880956bd0a67adf7cff8887d54afec994b5

--- init/init_d2lte.c
+++ init/init_d2lte.c
@@ -70,6 +70,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.product.device", "d2cri");
         property_set("telephony.lteOnCdmaDevice", "1");
         property_set("ro.telephony.default_network", "8");
+        property_set("ro.telephony.default_cdma_sub", "0");
     } else if (strstr(bootloader, "R530M")) {
         /* d2mtr */
         property_set("ro.build.fingerprint", "samsung/d2mtr/d2mtr:4.1.2/JZO54K/R530MVQAMF2:user/release-keys");
@@ -78,6 +79,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.product.device", "d2mtr");
         property_set("telephony.lteOnCdmaDevice", "1");
         property_set("ro.telephony.default_network", "8");
+        property_set("ro.telephony.default_cdma_sub", "0");
     } else if (strstr(bootloader, "L710")) {
         /* d2spr */
         property_set("ro.build.fingerprint", "samsung/d2spr/d2spr:4.1.2/JZO54K/L710VPBMD4:user/release-keys");
@@ -103,6 +105,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("telephony.lteOnCdmaDevice", "1");
         property_set("ro.telephony.default_network", "8");
         property_set("telephony.sms.pseudo_multipart", "1");
+        property_set("ro.telephony.default_cdma_sub", "0");
     } else if (strstr(bootloader, "I535")) {
         /* d2vzw */
         property_set("ro.build.fingerprint", "Verizon/d2vzw/d2vzw:4.3/JSS15J/I535VRUCML1:user/release-keys");
