20ad2cb255fc4ae1ac8edb641222671c5cf789a6
Dan Pasanen <dan.pasanen@gmail.com>
2014-03-05 21:24:37 -0600

    init_d2lte: remove dependency on android_device_qcom_common
    * we really don't need it, we can just define vendor_load_properties
    
    Change-Id: Ie3d2ba35232ebf407039320a874a1ae322b73f3d

---
+++ init/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := system/core/init
+LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
+LOCAL_SRC_FILES := init_d2lte.c
+LOCAL_MODULE := libinit_d2lte
+include $(BUILD_STATIC_LIBRARY)
