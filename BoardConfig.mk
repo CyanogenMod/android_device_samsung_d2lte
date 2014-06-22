dde733fd037bbaefb905782445ad0ca43282a0d5
Dave Daynard <nardholio@gmail.com>
2014-06-22 13:12:23 -0400

    Too many variants
    Change-Id: I53d443dda58c934cff41f66d17db08d8ef6b869f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,7 +103,7 @@ BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
 
 ifeq ($(filter cm_apexqtmo cm_expressatt cm_d2dcm,$(TARGET_PRODUCT)),)
 # Assert
-TARGET_OTA_ASSERT_DEVICE := d2att,d2spr,d2spi,d2tfnspr,d2tmo,d2cri,d2mtr,d2usc,d2vmu,d2vzw,d2lte
+TARGET_OTA_ASSERT_DEVICE := d2att,d2spr,d2spi,d2tfnspr,d2tmo,d2cri,d2mtr,d2usc,d2vmu,d2vzw,d2lte,d2can
 endif
 
 # Releasetools
