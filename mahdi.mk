aa77f2f6427a4ed262ff0d2c75d2ab6deaef102e
ChoznJuan <djg5826@gmail.com>
2014-09-05 22:49:51 -0400

    still trying to bring Mahdi to d2lte...added bootanimation to mahdi.mk

--- mahdi.mk
+++ mahdi.mk
@@ -6,4 +6,8 @@ $(call inherit-product, device/samsung/d2lte/full_d2lte.mk)
 # Inherit some common mahdi stuff.
 $(call inherit-product, vendor/mahdi/config/common_full_phone.mk)
 
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/mahdi/prebuilt/common/bootanimations/BOOTANIMATION-1280x768.zip:system/media/bootanimation.zip
+
 PRODUCT_NAME := mahdi_d2lte
