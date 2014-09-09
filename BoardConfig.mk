534a971c939acd0d3caabdeef9e3be7ac1e436c1
jt1134 <jt1134@gmail.com>
2014-09-09 11:58:15 -0700

    d2lte: include GPS HAL
    Change-Id: I37fac4a44f4b5673616a3dece65d50dab048a64f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,8 @@
 # inherit from common msm8960
 -include device/samsung/msm8960-common/BoardConfigCommon.mk
 
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/d2lte/include
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := d2att,d2spr,d2spi,d2tfnspr,d2tmo,d2cri,d2mtr,d2usc,d2vmu,d2vzw,d2lte,d2can
 
@@ -51,6 +53,9 @@ TARGET_ADD_ISO_MODE_1600 := true
 TARGET_NEED_CAMERA_ZSL := true
 TARGET_NEED_EXPOSURE_HACK := true
 TARGET_NEED_SAMSUNG_CAMERA_MODE := true
+
+# GPS
+TARGET_NO_RPC := true
 
 # RIL
 BOARD_RIL_CLASS := ../../../device/samsung/d2lte/ril
